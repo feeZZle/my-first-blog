29a27af5fdb13166acb976c50e0e6682ffe451fb
Your Name <farid.huseynlieng@gmail.com>
2020-12-17 16:43:24 +0400

    Changed the HTML for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-
-admin.site.register(Post)
+# to make our models visible on the admin page
+admin.site.register(Post) 
